b3a6c89f29885044164bb279f7653b8af786723e
Antônio Carlos <antoniocarlos.dev@gmail.com>
2017-10-05 19:34:09 +0800

    Configuring UICollectinViewFlowLayout properties

--- MemeMe/ViewControllers/SentMemesCollectionViewController.swift
+++ MemeMe/ViewControllers/SentMemesCollectionViewController.swift
@@ -12,6 +12,8 @@ private let reuseIdentifier = "memeItemCell"
 
 class SentMemesCollectionViewController: UICollectionViewController {
     
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
     var memes: [Meme] {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         return appDelegate.memes
@@ -19,13 +21,18 @@ class SentMemesCollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateFlowLayout(view.frame.size)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         collectionView?.reloadData()
     }
-
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        updateFlowLayout(size)
+    }
+    
     // MARK: UICollectionViewDataSource
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -36,6 +43,23 @@ class SentMemesCollectionViewController: UICollectionViewController {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! MemeCollectionViewCell
         cell.configureWith(meme: memes[indexPath.row])
         return cell
+    }
+    
+    // MARK: - Helpers
+    
+    private func updateFlowLayout(_ withSize: CGSize) {
+        
+        let landscape = withSize.width > withSize.height
+        
+        let space: CGFloat = landscape ? 5 : 3
+        let items: CGFloat = landscape ? 5 : 3
+        
+        // (total width - space gaps) / the remainder divided by the number of items presented
+        let dimension = (withSize.width - ((items - 1) * space)) / items
+        
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
     }
 
 }
